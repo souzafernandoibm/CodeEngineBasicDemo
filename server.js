bab8fa5e023cfac94a4d4ff3442c3222dbd9a81e
Doug Davis <dug@us.ibm.com>
2020-10-13 20:40:02 +0000

    more
    Signed-off-by: Doug Davis <dug@us.ibm.com>

--- server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const port = process.env.PORT || 8080;
-const app = express();
-
-app.get('/', (request, response) => { response.send(`Hello World!`); });
-
-app.listen(port);
